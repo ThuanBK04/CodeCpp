e1771712117f4eb9f959c6ebdea9b266353ceef2
NguyenDucThuan_TH <thuan.nd2709@gmail.com>
2024-06-11 23:56:54 +0700

    Practice a simple program

---
+++ WorkSpace1/Challenge_Section4/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int myFavoriteNum = 0;
+	cout << "Enter your favorite number between 1 and 100: ";
+    cin >> myFavoriteNum;
+    cout << "Amazing!! That's my favorite number too!" << endl;
+    cout << "No realy!!, " << myFavoriteNum << " is my favorite number!" << endl;
+	return 0;
+}
